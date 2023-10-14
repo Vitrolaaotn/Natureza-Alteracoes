96ac0c9df113fe52ab334d5d4661f1d12ba93485
Vitrola <vitrola.aotn@gmail.com>
2023-10-14 11:00:22 -0300

    Update

---
+++ index.js
@@ -0,0 +1,11 @@
+import {faq } from './modulo/faq';
+faq();
+import { imgclick } from './modulo/imgclick';
+imgclick();
+import { menuMobile } from './modulo/menuMobile';
+menuMobile();
+import { Modal } from './modulo/Modal';
+Modal();
+
+
+
